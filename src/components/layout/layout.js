dd05d51f44d0dc0ed270ce82144609f3d372a195
Clint Philbin <clint.philbin@gmail.com>
2023-02-28 17:37:02 -0800

    Add col styles

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,17 +1,19 @@
 import * as React from 'react'
-import { title, container } from './layout.module.css'
+import { title, container, col } from './layout.module.css'
 import Navigation from '../navigation/navigation'
 import Footer from '../footer/footer'
 
 const Layout = ({ pageTitle, children, hero = null }) => {
   return (
     <div className={ container }>
-      <Navigation>{hero}</Navigation>
-      <main>
-        <h1 className={ title }>{pageTitle}</h1>
-        {children}
-      </main>
-      <Footer/>
+      <div className={ col }>
+        <Navigation>{hero}</Navigation>
+        <main>
+          <h1 className={ title }>{pageTitle}</h1>
+          {children}
+        </main>
+        <Footer/>
+      </div>
     </div>
   )
 }
