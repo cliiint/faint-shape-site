40b04f2c73a781bc2271ca06fe9acc2b5380cb1d
Clint Philbin <clint.philbin@gmail.com>
2023-02-28 18:36:55 -0800

    Add icons to footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,11 +1,17 @@
 import * as React from 'react'
-import { footer, links } from './footer.module.css'
+import { links, link } from './footer.module.css'
+import { FaBandcamp } from "@react-icons/all-files/fa/FaBandcamp";
+import { FaInstagram } from "@react-icons/all-files/fa/FaInstagram";
+import { HiOutlineMail } from "@react-icons/all-files/hi/HiOutlineMail";
 
 const Footer = () => {
   return (
-    <footer className={footer}>
+    <footer>
       <ul className={links}>
-        <li>{new Date().getFullYear()}</li>
+        <li className={link}><a href="https://faintshape.bandcamp.com/" target="_blank"><FaBandcamp/></a></li>
+        <li className={link}><a href="https://www.instagram.com/faint_shape/" target="_blank"><FaInstagram/></a></li>
+        <li className={link}><a href="mailto:faintshape@gmail.com" target="_blank"><HiOutlineMail/></a></li>
+        <li className={link}>{new Date().getFullYear()}</li>
       </ul>
     </footer>
   )
