8a340b375b07e268d658efa9ef006038915ec23f
Clint Philbin <clint.philbin@gmail.com>
2023-04-18 19:47:53 -0700

    Shows row index (#5)
    * Display shows as rows on index
    
    * Format date string on client
    
    * Add hide/show past shows

--- src/components/shows/shows.js
+++ src/components/shows/shows.js
@@ -1,16 +1,19 @@
 import * as React from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
-import { list } from './shows.module.css'
+import { list, row, tickets, day, flex, boxOffice, pastShows } from './shows.module.css'
+import { useState } from 'react'
 
 const Shows = () => {
   const query = useStaticQuery(graphql`
     query {
-      allContentfulShow(sort: {date: DESC}) {
+      allContentfulShow(sort: {date: ASC}) {
         edges {
           node {
             id
             title
             slug
+            location
+            ticketsUrl
             description {
               raw
             }
@@ -32,27 +35,42 @@ const Shows = () => {
   const now = new Date();
   let past = [];
   let upcoming = [];
+  const [showPast, setShowPast] = useState(false);
 
   shows.forEach(show => {
     const endOfShowDay = new Date(show.date).setUTCHours(23,59,59,999);
     endOfShowDay > now ? upcoming.push(show) : past.push(show);
   });
 
+  const ticketsUrl = (show) => <a className={`${tickets} button`} href={show.ticketsUrl} target="_blank">Tickets</a>;
+  const box = <p className={`${tickets} ${boxOffice}`}>Box Office</p>;
+  const date = (utc) => {
+    let formatted = new Date(utc).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
+    return formatted.substring(formatted.indexOf(',') + 2);
+  };
+
+  const handleClick = () => setShowPast(!showPast);
+
   const showList = (events, heading) => {
     return (
       <>
         <h2>{heading}</h2>
-        <ul className={list}>
+        <div className={list}>
           {events.map(show => {
             return (
-              <li key={show.id}>
-                <Link to={`/shows/${show.slug}`}>
-                  <img src={show.image.file.url} alt={show.image.title}/>
-                </Link>
-              </li>
+              <div className={row} key={show.id}>
+                <div className={day}>
+                  <p>{date(show.date)}</p>
+                  <p>{show.location}</p>
+                </div>
+                <div className={flex}>
+                  <Link to={`/shows/${show.slug}`}>Details</Link>
+                  {show.ticketsUrl ? ticketsUrl(show) : box}
+                </div>
+              </div>
             );
           })}
-        </ul>
+        </div>
       </>
     )
   };
@@ -61,7 +79,10 @@ const Shows = () => {
     <>
       <h1>Shows</h1>
       {upcoming.length > 0 && showList(upcoming, 'Upcoming')}
-      {past.length > 0 && showList(past, 'Past')}
+      <p className={pastShows} onClick={handleClick}>
+        {showPast ? 'Hide past shows' : 'See past shows'}
+      </p>
+      {past.length > 0 && showPast && showList(past.reverse(), 'Past')}
     </>
   )
 }
