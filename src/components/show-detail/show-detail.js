9c29fab2d557bbfc30f71c553fb529fb838e0be1
Clint Philbin <clint.philbin@gmail.com>
2026-08-12 16:35:22 -0700

    Add pagination t o show detail

--- src/components/show-detail/show-detail.js
+++ src/components/show-detail/show-detail.js
@@ -1,9 +1,10 @@
 import * as React from 'react'
 import Layout from '../layout/layout'
 import { renderRichText } from 'gatsby-source-contentful/rich-text'
-import { flex, content } from './show-detail.module.css'
+import { flex, content, pagination, disabled } from './show-detail.module.css'
+import { Link } from 'gatsby';
 
-function ShowDetail({ show }) {
+function ShowDetail({ show, prevShow, nextShow }) {
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
   return (
@@ -12,6 +13,18 @@ function ShowDetail({ show }) {
       <div className={flex}>
         <img src={show.image.file.url} alt={show.image.title}/>
         <div className={content}>{renderRichText(show.description, {})}</div>
+      </div>
+      <div className={pagination}>
+        {prevShow ? (
+          <Link to={`/shows/${prevShow.slug}`}>&#9664; Previous</Link>
+        ) : (
+          <span className={disabled}>&#9664; Previous</span>
+        )}
+        {nextShow ? (
+          <Link to={`/shows/${nextShow.slug}`}>Next &#9658;</Link>
+        ) : (
+          <span className={disabled}>Next &#9658;</span>
+        )}
       </div>
     </Layout>
   )
